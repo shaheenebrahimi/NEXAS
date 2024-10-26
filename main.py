e0b58d01d2bf4a9442bd69da012747d9973fc024
shaheenebrahimi <shaheen.m.ebrahimi@gmail.com>
2024-10-25 23:35:36 -0500

    Initial setup and class structuring

--- main.py
+++ main.py
@@ -1,29 +1,75 @@
+from dotenv import load_dotenv
 from groq import Groq
 import os
-from dotenv import load_dotenv
 
-load_dotenv()
-groq_api_key = os.getenv("GROQ_API_KEY")
+class NEXAS:
 
-client = Groq(api_key=groq_api_key)
-completion = client.chat.completions.create(
-    model="llama-3.2-3b-preview",
-    messages=[
+    model_options = [
+        "llama-3.2-3b-preview"
+    ]
+
+    def __init__(self, model):
+        load_dotenv()
+        self.client = Groq(api_key=os.getenv("GROQ_API_KEY"))
+        self.model = model
+        self.prompt = """## Role:
+        You are a helpful assistant.
+
+        ## Objective:
+        You will be given a user utterance as input. Using the utterance classify the intention of the action. Then service the action accordingly.
+
+        ## Classes:
+        ```json
         {
-            "role": "system",
-            "content": "given an input. write a haiku"
+            "Class Name": "Query",
+            "Class Content": Respond to the query,
         },
         {
-            "role": "user",
-            "content": "moon"
+            "Class Name": "Action",
+            "Class Content": Respond with what actions you would need to perform if you had access to a robot to control
         }
-    ],
-    temperature=1,
-    max_tokens=1024,
-    top_p=1,
-    stream=True,
-    stop=None,
-)
+        ```
 
-for chunk in completion:
-    print(chunk.choices[0].delta.content or "", end="")
+        ## Instructions:
+        1. Identify which of the classes the utterance belongs to
+        2. Depending on the class answer the class content
+
+        ## Output:
+        ```json
+        {
+            "class": selected class,
+            "content": content corresponding to class
+        }
+        ```
+        """
+        
+    def query(self, query):
+        completion = self.client.chat.completions.create(
+            model=self.model,
+            messages=[
+                {
+                    "role": "system",
+                    "content": self.prompt
+                },
+                {
+                    "role": "user",
+                    "content": query
+                }
+            ],
+            temperature=1,
+            max_tokens=1024,
+            top_p=1,
+            stream=True,
+            stop=None,
+        )
+
+        for chunk in completion:
+            print(chunk.choices[0].delta.content or "", end="")
+
+def main():
+    assistant = NEXAS(model="llama-3.2-3b-preview")
+    assistant.query("What is the weather today")
+
+if __name__ == "__main__":
+    main()
+
